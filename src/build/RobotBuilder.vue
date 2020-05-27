11124b36eef0f25d12292e62cbeb6623fbe49f77
Boyang Li <nian1538@gmail.com>
2020-05-27 22:12:59 +0800

    reducing duplication with mixins

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -58,6 +58,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -71,6 +72,9 @@ function getNextValidIndex(index, length) {
 
 export default {
   name: 'Robotbuilder',
+  created() {
+    console.log('component created');
+  },
   data() {
     return {
       availableParts,
@@ -82,6 +86,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
@@ -173,15 +178,17 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .part {
   position: relative;
   width:165px;
   height:165px;
   border: 3px solid #aaa;
 }
-.part img {
-  width:165px;
+.part {
+  img {
+    width:165px;
+  }
 }
 .top-row {
   display:flex;
