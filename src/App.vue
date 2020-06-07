ac31dba32830164b7d946913ed308df6a25fe8cd
Boyang Li <nian1538@gmail.com>
2020-06-07 19:45:24 +0800

    navigating from code

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,28 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png" />
-            Build-a-bot
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png" />
+              Build-a-bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
     </header>
     <main>
-        <RobotBuilder />
+      <router-view />
     </main>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-
 export default {
   name: 'App',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
@@ -71,5 +72,12 @@ ul {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
