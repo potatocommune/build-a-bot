d39646827a0786aabe610cc1b778b18558b96e62
Boyang Li <nian1538@gmail.com>
2020-06-13 20:40:31 +0800

    enabling HTML5 history mode

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -83,6 +83,16 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'Robotbuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   created() {
     console.log('component created');
@@ -90,6 +100,7 @@ export default {
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -118,6 +129,7 @@ export default {
       + robot.rightArm.cost
       + robot.base.cost;
       this.cart.push({ ...robot, cost });
+      this.addedToCart = true;
     },
   },
 };
