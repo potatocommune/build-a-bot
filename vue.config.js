ebffab66312eeae24ab5aa00eef78b89e30cae67
Boyang Li <nian1538@gmail.com>
2020-07-04 21:59:37 +0800

    deploying vue applications to production

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  configureWebpack: (config) => {
+    config.module.rules.push({
+      test: /\.coffee$/,
+      use: ['coffee-loader'],
+    });
+
+    const newRule = {
+      test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+      use: [
+        {
+          loader: 'C:\\Users\\nian1\\Desktop\\build-a-bot\\node_modules\\url-loader\\dist\\cjs.js',
+          options: {
+            limit: 5000,
+            fallback: {
+              loader: 'C:\\Users\\nian1\\Desktop\\build-a-bot\\node_modules\\file-loader\\dist\\cjs.js',
+              options: {
+                name: 'img/[name].[hash:8].[ext]',
+              },
+            },
+          },
+        },
+      ],
+    };
+
+    const imagesRuleIndex = config.module.rules
+      .findIndex((x) => x.test.source.includes('png|jpe?g|gif'));
+
+    config.module.rules.splice(imagesRuleIndex, 1, newRule);
+  },
+
   devServer: {
     proxy: {
       '/api': {
